90353dc66c526fbd7611fb9df376969642a0b6dc
keyasanyal <keya.sanyal07@gmail.com>
2018-12-04 23:36:35 -0500

    dependencies

--- src/store.js
+++ src/store.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.config.productionTip = false
+Vue.use(Vuex);
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+export default new Vuex.Store({
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+});
