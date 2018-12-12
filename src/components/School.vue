9fdd4c56f5803cd45baa772368dfe8ce4d7d1c77
keyasanyal <keya.sanyal07@gmail.com>
2018-12-11 21:17:20 -0500

    more changes

--- src/components/School.vue
+++ src/components/School.vue
@@ -1,32 +1,41 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+      <body>
+
+  <img src="img/logo2.png" class="logo">
+
+  <h1 class="pageTitle">All Tasks</h1>
+
+
+
+<h2>December 5th, 2018</h2>
+
+<main class="task-list">
+
+  <label class="container fix3">Complete questions 1-12 pg.342
+    <input type="checkbox" checked="checked">
+    <span class="checkmark"></span>
+</label>
+
+    <label class="container fix3">Finish essay
+      <input type="checkbox" checked="checked">
+      <span class="checkmark"></span>
+</label>
+
+</main>
+
+<div class="nav">
+<a  href="firsttasks.html"><button class="btn1"><i  class="fa fa-home"></i></button></a>
+<a  href="calendar.html"><button class="btn2"><i  class="fa fa-calendar"></i></button></a>
+<a  href="addtasks.html"><button class="btn3"><i  class="fa fa-plus"></i></button></a>
+<a  href="work.html"><button class="btn4"><i  class="fa fa-suitcase"></i></button></a>
+<a  href="school.html" ><button class="btn5"><i  class="fa fa-pencil"></i></button></a>
+</div>
+
+<a name="workTasks"></a>
+
+</body>
   </div>
 </template>
 
